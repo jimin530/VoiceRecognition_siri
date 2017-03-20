4417a991ee8d4bd6df677180fb7891e142b1fd7d
Jimin Kim <jimin6877@gmail.com>
2017-03-20 13:06:27 +0900

    update 2017.03.20

--- app/src/main/java/com/jimin/recovoice/MainActivity.java
+++ app/src/main/java/com/jimin/recovoice/MainActivity.java
@@ -1,19 +1,26 @@
 package com.jimin.recovoice;
 
+import android.Manifest;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+    Button button, button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // permission request
+        request_read_audio();
 
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -22,14 +29,67 @@ public class MainActivity extends AppCompatActivity {
                 startService(new Intent(getApplicationContext(), SpottingService.class)); // Start CMUSphinx by Service
             }
         });
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(getApplicationContext(), SpottingService.class)); // Start CMUSphinx by Service
+            }
+        });
     }
+
     @Override
     protected void onDestroy() {
         try {
+            // stop all Services
             stopService(new Intent(getApplicationContext(), SpottingService.class));
             stopService(new Intent(getApplicationContext(), SttService.class));
         } catch (Exception e) {
         }
         super.onDestroy();
+    }
+
+    // 퍼미션/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    request_read_audio();
+                } else {
+                    finish();
+                }
+            }
+            case 2: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    request_write_external_storage();
+                } else {
+                    finish();
+                }
+            }
+        }
+    }
+
+    public void request_read_audio() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO)) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+            }
+        } else {
+        }
+    }
+
+    public void request_write_external_storage() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+            }
+        } else {
+        }
     }
 }
